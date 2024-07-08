9878932d3c08c443648376ff53221fbfb7cdd3a3
sulla <sultan.yergaliyev@outlook.com>
2024-07-08 11:17:16 +0100

    modified controller.py, added create user functionality

---
+++ microservices/user-service/src/api/controller.py
@@ -0,0 +1,8 @@
+from . import crud
+from schema import user_schema
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+
+def create_user(db: Session, user_create: user_schema.UserRequest):
+    return crud.create_user(db=db, user_create=user_create)
