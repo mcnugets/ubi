c4e9dec64d5d4e0ae453b7e2678f12664607e40e
sulla <sultan.yergaliyev@outlook.com>
2024-07-15 08:51:16 +0100

    -

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='shared_db_connection',
     version='0.1.0', 
-    packages=['database'],
+    packages=find_packages(),
     description='shared database connection',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
