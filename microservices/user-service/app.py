94c69f6d70a4fda1f78948b06406ebb968fdd810
sulla <sultan.yergaliyev@outlook.com>
2024-07-21 17:43:02 +0100

    add functionality to app.py

--- microservices/user-service/app.py
+++ microservices/user-service/app.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI, Depends
 from .src.api.routes import router
 from .dependencies import is_valid_token
 
+from .src.models.user import base
+from shared_database.connection import db_session, engine
+
 # TODO: create generative token for authentication
 # TODO: create proper sha256 hashing to hash password
+
+base.metadata.create_all(bind=engine)
 app = FastAPI()  # token param missing
 
 
 app.include_router(router=router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Can confirm this shit works"}
