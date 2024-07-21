6c3ef5bc82c61c2354ad74eb5ff1232e18f21900
sulla <sultan.yergaliyev@outlook.com>
2024-07-21 17:54:34 +0100

    modify routes.py file

--- microservices/user-service/src/api/routes.py
+++ microservices/user-service/src/api/routes.py
@@ -1,14 +1,22 @@
 from . import controller
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from models import user
-from schema import user_schema
+from ..models import user
+from ..schema import user_schema
 from fastapi import Depends
-from .....shared_components.connection import get_db
-router = APIRouter(
-    prefix="/user", tags=["user"], responses={404, {"description": "not found"}}
-)
+from shared_database.connection import get_db
 
+
+router = APIRouter(
+    prefix="/user", tags=["user"], responses={404: {"description": "Not found"}}
+    )
+
+
+# router = APIRouter(
+#     prefix="/user",
+#     tags=["user"],
+#     responses={404: {"description": "not found"}}
+# )
 
 @router.post("/create_user")
 def create_user(user: user_schema.UserRequest, db: Session = Depends(get_db)):
